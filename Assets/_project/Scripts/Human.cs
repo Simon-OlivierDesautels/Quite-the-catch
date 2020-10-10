fccc44a2572d2656c6ee0b446736584ffb4500df
Simon-Olivier <simonolivier.desautels@gmail.com>
2020-10-09 22:21:23 -0400

    Initial commit

---
+++ Assets/_project/Scripts/Human.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour
+{
+//    public string PlayerControls => _controlHorizontal; could be private or deleted
+    public float PlayerAxis => _playerAxis;
+    
+    [Header("Controls info")] 
+    [SerializeField] private PlayerNumber _playerNumber;
+    [Header("Running")] 
+    [SerializeField] int _speed;
+    [Header("Jumping")] 
+    [SerializeField] [Range(0, 10)] int _jumpForce;
+    [SerializeField] private LayerMask _groundMask;
+    [Header("Debug")] [SerializeField] private bool _debug;
+    [SerializeField] private float _gizmosLength;
+    [SerializeField] private Vector3 colliderOffset;
+
+    private enum PlayerNumber
+    {
+        Player1,
+        Player2
+    };
+
+    private Rigidbody2D Rigidbody2D;
+    private string _controlHorizontal;
+    private float _playerAxis;
+    private bool isGrounded;
+
+    protected virtual void Awake()
+    {
+        switch (_playerNumber)
+        {
+            case PlayerNumber.Player1:
+                _controlHorizontal = "Horizontal.0";
+                break;
+
+            case PlayerNumber.Player2:
+                _controlHorizontal = "Horizontal.1";
+                break;
+        }
+
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    protected void HorizontalMovement()
+    {
+        _playerAxis = Input.GetAxis(_controlHorizontal);
+        float _deltaX = Input.GetAxis(_controlHorizontal) * _speed;
+        Vector2 playerVelocity = new Vector2(_deltaX, Rigidbody2D.velocity.y);
+        Rigidbody2D.velocity = playerVelocity;
+    }
+
+    protected void VerticalMovement()
+    {
+        GroundCollisionDetection();
+        if (Input.GetButton("Fire2") && isGrounded)
+        {
+            Rigidbody2D.AddForce(Vector2.up * (_jumpForce * 10));
+        }
+    }
+
+    protected void GroundCollisionDetection()
+    {
+        isGrounded = Physics2D.Raycast(transform.position + colliderOffset, Vector3.down, _gizmosLength, _groundMask);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_debug)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position + colliderOffset,
+                transform.position + colliderOffset + (Vector3.down * _gizmosLength));
+        }
+    }
+}
